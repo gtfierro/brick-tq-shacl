1258402083bf1a7f08452ae0279983891e279e6e
Gabe Fierro <gtfierro@mines.edu>
2024-10-22 14:37:51 -0600

    try to check java at import time

--- brick_tq_shacl/__init__.py
+++ brick_tq_shacl/__init__.py
@@ -1,3 +1,15 @@
 from . import topquadrant_shacl
-__version__ = "0.2.0"
+__version__ = "0.3.4a2"
 __all__ = ["topquadrant_shacl"]
+
+
+def check_java_installed():
+    import subprocess
+    import sys
+    try:
+        subprocess.run(["java", "-version"], check=True, capture_output=True)
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        sys.exit("Java is not installed. Please install Java to use this package.")
+
+
+check_java_installed()
